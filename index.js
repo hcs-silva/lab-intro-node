784a1662a08c13ac9b1861e6b23f5583400f6244
Hernâni Silva <hcsilvamatos@hotmail.com>
2024-10-25 22:28:36 +0100

    4 errors remaining

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {
+      this.items.push(item);
+    }
 
-  get(pos) {}
+    this.length = this.items.length;
 
-  max() {}
+    if (this.length < 3) {
+      this.items.push(item);
+    }
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    } else return this.items[pos];
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return this.items[this.items.length - 1];
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return this.items[0];
+  }
+
+  sum() {
+    const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+    if (this.items.length === 0) {
+      return 0;
+    } else return sum;
+  }
+
+  avg() {
+    const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else return Math.round(sum / this.items.length);
+  }
 }
 
 module.exports = SortedList;
